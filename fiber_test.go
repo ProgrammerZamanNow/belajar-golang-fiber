5b9c6178d0fbb7067f1192a7b15279beab3ec00d
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-23 21:58:42 +0700

    route parameter

--- fiber_test.go
+++ fiber_test.go
@@ -69,3 +69,20 @@ func TestHttpRequest(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello Eko Khannedy", string(bytes))
 }
+
+func TestRouteParameter(t *testing.T) {
+	app.Get("/users/:userId/orders/:orderId", func(ctx *fiber.Ctx) error {
+		userId := ctx.Params("userId")
+		orderId := ctx.Params("orderid")
+		return ctx.SendString("Get Order " + orderId + " From User " + userId)
+	})
+
+	request := httptest.NewRequest("GET", "/users/eko/orders/10", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Get Order 10 From User eko", string(bytes))
+}
