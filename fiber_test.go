7fd5485101dd53209a325eaa858e940f38faa6c1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-24 14:47:30 +0700

    view

--- fiber_test.go
+++ fiber_test.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/mustache/v2"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"mime/multipart"
@@ -15,7 +16,10 @@ import (
 	"testing"
 )
 
+var engine = mustache.New("./template", ".mustache")
+
 var app = fiber.New(fiber.Config{
+	Views: engine,
 	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
 		ctx.Status(fiber.StatusInternalServerError)
 		return ctx.SendString("Error : " + err.Error())
@@ -335,4 +339,25 @@ func TestErrorHandler(t *testing.T) {
 	bytes, err := io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, "Error : ups", string(bytes))
+}
+
+func TestView(t *testing.T) {
+	app.Get("/view", func(ctx *fiber.Ctx) error {
+		return ctx.Render("index", fiber.Map{
+			"title":   "Hello Title",
+			"header":  "Hello Header",
+			"content": "Hello Content",
+		})
+	})
+
+	request := httptest.NewRequest("GET", "/view", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Contains(t, string(bytes), "Hello Title")
+	assert.Contains(t, string(bytes), "Hello Header")
+	assert.Contains(t, string(bytes), "Hello Content")
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.21.1
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/cbroglie/mustache v1.4.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gofiber/fiber/v2 v2.50.0 // indirect
+	github.com/gofiber/template v1.8.2 // indirect
+	github.com/gofiber/template/mustache/v2 v2.0.7 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
