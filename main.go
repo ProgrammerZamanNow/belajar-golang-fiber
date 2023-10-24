d75bf6bd81d287210471baef23914bb32f064c77
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-24 15:31:04 +0700

    middleware

--- main.go
+++ main.go
@@ -14,7 +14,14 @@ func main() {
 		Prefork:      true,
 	})
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
+	app.Use("/api", func(ctx *fiber.Ctx) error {
+		fmt.Println("I'm middleware before processing request")
+		err := ctx.Next()
+		fmt.Println("I'm middleware after processing request")
+		return err
+	})
+
+	app.Get("/api/hello", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello World")
 	})
 
