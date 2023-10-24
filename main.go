d4f40dff9188ae3b4bd2395d13a940ba74297b4f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-24 13:41:25 +0700

    prefork

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"time"
 )
@@ -10,11 +11,18 @@ func main() {
 		IdleTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
 		ReadTimeout:  time.Second * 5,
+		Prefork:      true,
 	})
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello World")
 	})
+
+	if fiber.IsChild() {
+		fmt.Println("I'm child process")
+	} else {
+		fmt.Println("I'm parent process")
+	}
 
 	err := app.Listen("localhost:3000")
 	if err != nil {
