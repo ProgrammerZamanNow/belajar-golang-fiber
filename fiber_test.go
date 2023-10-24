dfc17790ec4ca8635df6946f5bdfd93b71dad4f0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-24 13:30:53 +0700

    static

--- fiber_test.go
+++ fiber_test.go
@@ -302,3 +302,16 @@ func TestRoutingGroup(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello World", string(bytes))
 }
+
+func TestStatic(t *testing.T) {
+	app.Static("/public", "./source")
+
+	request := httptest.NewRequest("GET", "/public/contoh.txt", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "this is sample file for upload", string(bytes))
+}
